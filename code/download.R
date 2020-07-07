b43aa9155422bc291493a3ca57a5c724d637d4e6
lincoln colling <lincoln@colling.net.nz>
2020-07-07 22:08:11 +0100

    pushed dockerfile

--- code/download.R
+++
@@ -1,7 +0,0 @@
-# Install ODP helper
-remotes::install_github("ljcolling/ODPHelper", ref = "dev")
-xfun::pkg_attach2("osfr")
-
-yousif_download_info <- ODPHelper::download_yousif()
-dir.create("made")
-saveRDS(object = yousif_download_info, file = "made/yousif_download_info.Rdata")
