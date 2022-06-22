95cabc5cbb5ddda4a18278bf678565e233c3cb7f
ljcolling <lincoln@colling.net.nz>
2022-06-22 12:10:49 +0000

    Deploying to gh-pages from  @ bce662ac0247d4d91e8698125e3500608342025b 🚀

--- code/download.R
+++
@@ -1,6 +0,0 @@
-
-xfun::pkg_attach2("osfr")
-
-yousif_download_info <- ODPHelper::download_yousif()
-dir.create("made")
-saveRDS(object = yousif_download_info, file = "made/yousif_download_info.Rdata")
